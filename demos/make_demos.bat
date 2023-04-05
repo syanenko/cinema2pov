7347b33a116f54663f05601906e7d6ebbe9627fc
Sergey Yanenko <syanenko65@gmail.com>
2023-04-06 00:42:01 +0300

    Demo: stone

--- demos/make_demos.bat
+++ demos/make_demos.bat
@@ -1,20 +1,28 @@
 REM //
-REM // Converting demo scenes
+REM // Making demo scenes
 REM //
 
 REM // Set path to POV-Ray location
 SET POVRAY="C:\Program Files\POV-Ray\v3.8-beta\bin\pvengine64.exe"
 
-REM..\cinema2pov\cinema2pov.exe mat_wood.c4d mat_wood.inc >> make_demos.log
-REM%POVRAY% demos.ini /RENDER mat_wood.pov /EXIT
-
-REM..\cinema2pov\cinema2pov.exe mat_stone.c4d mat_stone.inc >> make_demos.log
-REM%POVRAY% demos.ini /RENDER mat_stone.pov /EXIT
-
+REM - Glass
 REM..\cinema2pov\cinema2pov.exe mat_glass.c4d mat_glass.inc >> make_demos.log
 REM%POVRAY% demos.ini /RENDER mat_glass.pov /EXIT
 
-REM // Test
-..\cinema2pov\cinema2pov.exe mat_wood.c4d mat_wood.inc >> make_demos.log
-%POVRAY% /RENDER mat_wood.pov
+REM - Wood
+REM..\cinema2pov\cinema2pov.exe mat_wood.c4d mat_wood.inc >> make_demos.log
+REM%POVRAY% demos.ini /RENDER mat_wood.pov /EXIT
+
+REM - Stone
+REM..\cinema2pov\cinema2pov.exe mat_stone.c4d mat_stone.inc >> make_demos.log
+REM%POVRAY% demos.ini /RENDER mat_stone.pov /EXIT
+
+
+REM // Test wood
+REM..\cinema2pov\cinema2pov.exe mat_wood.c4d mat_wood.inc >> make_demos.log
+REM%POVRAY% /RENDER mat_wood.pov
+
+REM // Test stone
+..\cinema2pov\cinema2pov.exe mat_stone.c4d mat_stone.inc >> make_demos.log
+%POVRAY% /RENDER mat_stone.pov
 
